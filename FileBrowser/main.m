4138d80e34d95e4709f7febdacb0a4b09d32b6ec
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2013-06-18 15:02:42 -0700

    Initial Commit

---
+++ FileBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FileBrowser
+//
+//  Created by Steven Troughton-Smith on 18/06/2013.
+//  Copyright (c) 2013 High Caffeine Content. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([FBAppDelegate class]));
+	}
+}
---
+++ FileBrowserTests/FileBrowserTests.m
@@ -0,0 +1,36 @@
+//
+//  FileBrowserTests.m
+//  FileBrowserTests
+//
+//  Created by Steven Troughton-Smith on 18/06/2013.
+//  Copyright (c) 2013 High Caffeine Content. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FileBrowserTests : XCTestCase
+
+@end
+
+@implementation FileBrowserTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
