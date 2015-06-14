1f945aec0099ebfd9a2f756e00ea60bc56255455
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2015-06-14 02:44:59 +0100

    Initial Native Watch App

---
+++ NanoFileBrowserFramework/FBFrameworkMain.m
@@ -0,0 +1,9 @@
+//
+//  FBFrameworkMain.m
+//  FileBrowser
+//
+//  Created by Steven Troughton-Smith on 13/06/2015.
+//  Copyright © 2015 High Caffeine Content. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
